688397f98aa0acab0d5021e7143732541aefd7b4
hunderte <test@qq.com>
2026-05-09 16:19:37 +0800

    update data

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,123 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "braham2022self": {
+        "author": "Braham, Nassim Ait Ali and Mou, Lichao and Chanussot, Jocelyn and Mairal, Julien and Zhu, Xiao Xiang",
+        "booktitle": "IGARSS 2022-2022 IEEE international geoscience and remote sensing symposium",
+        "keywords": "Hyperspectral Image Classification, Self Supervised Learning, Few Shot Classification",
+        "organization": "IEEE",
+        "pages": "267--270",
+        "title": "Self supervised learning for few shot hyperspectral image classification",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "chen2021self": {
+        "author": "Chen, Da and Chen, Yuefeng and Li, Yuhong and Mao, Feng and He, Yuan and Xue, Hui",
+        "booktitle": "ICASSP 2021-2021 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP)",
+        "keywords": "Few-shot learning, Self-supervised learning, Metric learning, Cross-domain",
+        "organization": "IEEE",
+        "pages": "1745--1749",
+        "title": "Self-supervised learning for few-shot image classification",
+        "type": "inproceedings",
+        "year": "2021"
+    },
+    "li2020deep": {
+        "author": "Li, Yu and Zhang, Lei and Wei, Wei and Zhang, Yanning",
+        "booktitle": "IGARSS 2020-2020 IEEE International Geoscience and Remote Sensing Symposium",
+        "keywords": "few-shot, deep learning, HSI classifica\u0002tion, self-supervised task",
+        "organization": "IEEE",
+        "pages": "501--504",
+        "title": "Deep self-supervised learning for few-shot hyperspectral image classification",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "li2020few": {
+        "author": "Li, Jianyi and Liu, Guizhong",
+        "journal": "arXiv preprint arXiv:2008.09942",
+        "keywords": "few shot learning, contrastive self-supervised learning",
+        "title": "Few-shot image classification via contrastive self-supervised learning",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "year": "2020"
+    },
+    "li2021scl": {
+        "author": "Li, Xiaomin and Shi, Daqian and Diao, Xiaolei and Xu, Hao",
+        "journal": "IEEE Transactions on Geoscience and Remote Sensing",
+        "keywords": "Feature representation, few-shot learning, remote sensing (RS) scene classification, self-supervised con\u0002trastive learning (SCL)",
+        "pages": "1--12",
+        "publisher": "IEEE",
+        "title": "SCL-MLNet: Boosting few-shot remote sensing scene classification via self-supervised contrastive learning",
+        "type": "article",
+        "volume": "60",
+        "year": "2021"
+    },
+    "li2023few": {
+        "author": "Li, Zhaokui and Guo, Hui and Chen, Yushi and Liu, Cuiwei and Du, Qian and Fang, Zhuoqun and Wang, Yan",
+        "journal": "IEEE Transactions on Geoscience and Remote Sensing",
+        "keywords": "Contrastive learning (CL), few-shot learning(FSL), hyperspectral image (HSI) classification, self-supervised learning (SSL)",
+        "pages": "1--17",
+        "publisher": "IEEE",
+        "title": "Few-shot hyperspectral image classification with self-supervised learning",
+        "type": "article",
+        "volume": "61",
+        "year": "2023"
+    },
+    "liang2025self": {
+        "author": "Liang, Jingyong and Meyer, Bernd and Lee, Issac Ning and Do, Thanh-Toan",
+        "booktitle": "ICASSP 2025-2025 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP)",
+        "keywords": "Self-supervised learning, Few-Shot Learning, Acoustics, Bioacoustics",
+        "organization": "IEEE",
+        "pages": "1--5",
+        "title": "Self-supervised learning for acoustic few-shot classification",
+        "type": "inproceedings",
+        "year": "2025"
+    },
+    "liu2023category": {
+        "author": "Liu, Quanyong and Peng, Jiangtao and Chen, Na and Sun, Weiwei and Ning, Yujie and Du, Qian",
+        "journal": "IEEE Transactions on Geoscience and Remote Sensing",
+        "keywords": "Contrastive learning (CL), few-shot learning(FSL), hyperspectral image (HSI) classification, prototypical network",
+        "pages": "1--16",
+        "publisher": "IEEE",
+        "title": "Category-specific prototype self-refinement contrastive learning for few-shot hyperspectral image classification",
+        "type": "article",
+        "volume": "61",
+        "year": "2023"
+    },
+    "ping2024few": {
+        "author": "Ping, Li and Zhenyu, Yang",
+        "booktitle": "2024 International Conference on Cyber-Enabled Distributed Computing and Knowledge Discovery (CyberC)",
+        "keywords": "few-shot learning, self-supervised learning, feature fusion, self-attention",
+        "organization": "IEEE",
+        "pages": "44--51",
+        "title": "Few-shot Image Classification Method with Label Consistent and Inconsistent Self-supervised Learning",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "shaikh2024deep": {
+        "author": "Shaikh, MZ and Singh, Samender and Varshney, Neeraj and Saraswat, Birendra Kumar",
+        "booktitle": "2024 2nd International Conference on Disruptive Technologies (ICDT)",
+        "keywords": "Outperforms, Unlabeled, Utilizes, Medical, Results",
+        "organization": "IEEE",
+        "pages": "693--698",
+        "title": "Deep Learning-Based Self-Supervised Transfer Learning for Medical Image Classification",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "tian2020rethinking": {
+        "author": "Tian, Yonglong and Wang, Yue and Krishnan, Dilip and Tenenbaum, Joshua B and Isola, Phillip",
+        "booktitle": "European conference on computer vision",
+        "keywords": "few-shot learning, meta-learning, image classification, representation learning",
+        "organization": "Springer",
+        "pages": "266--282",
+        "title": "Rethinking few-shot image classification: a good embedding is all you need?",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "xiao2024self": {
+        "author": "Xiao, Yuyang",
+        "booktitle": "2024 International Conference on Image Processing, Computer Vision and Machine Learning (ICICML)",
+        "keywords": "Self-supervised learning, Few-shot classification, ResNet-101, Mini-ImageNet",
+        "organization": "IEEE",
+        "pages": "1818--1823",
+        "title": "Self-Supervised Learning in Deep Networks: A Pathway to Robust Few-Shot Classification",
+        "type": "inproceedings",
+        "year": "2024"
     }
 };
